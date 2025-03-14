3415df458d1b435107642113aa76d5c3651aab04
gsdsan10 <161904157+gsdsan10@users.noreply.github.com>
2025-03-13 21:24:56 -0400

    DESENVOLVIMENTO DO SITE PITZZARIA GGN 5.0
    Desenvolvimento pitzzaria

---
+++ Models/UsuarioModel.cs
@@ -0,0 +1,15 @@
+
+
+namespace pizzariaggn.models;
+
+public class Usuario {
+
+public int id {get; set;}
+
+public  string? Nome {get; set;}
+
+public string? Email {get; set;}
+
+
+}
+
---
+++ Repository/UserRepository.cs
@@ -0,0 +1,10 @@
+/* Using AventuraGames.Models;
+Using Npgsql;
+
+namespace AventuraGames.Repository;
+
+public class UserRepository{
+
+    private readonly DatabaseConnection _dbconnection;
+
+} */
